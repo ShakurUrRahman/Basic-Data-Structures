e5d6720cb17955fd06662cebbcaf709f2dcda8b2
shakururrahman <shakururrahman@gmail.com>
2023-07-17 22:31:34 +0600

    revise 3

--- J_Count_Letters.cpp
+++ J_Count_Letters.cpp
@@ -1,7 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-    
+
+
+void countOccurrences(const string& S) {
+    const int size = 26;
+    vector<int> occurrences(size, 0);
+
+    for (char ch : S) {
+        occurrences[ch - 'a']++;
+    }
+
+    for (int i = 0; i < size; ++i) {
+        if (occurrences[i] > 0) {
+            char letter = static_cast<char>('a' + i);
+            cout << letter << " : " << occurrences[i] << endl;
+        }
+    }
+}
+
+int main() {
+    string S;
+    cin >> S;
+
+    countOccurrences(S);
+
     return 0;
 }
